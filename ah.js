6007c69a25c521908d68ec02cfd8897c8348ffa0
9os <druidboykoston@gmail.com>
2017-01-03 02:08:01 -0600

    Add files via upload

--- ah.js
+++ ah.js
@@ -1,9 +1,13 @@
 var charList = ["Manathasf", "Siyothasf", "Gyromaxsee", "Gezbekdsf", "Spitspazd", "Shifvshank", "Nazgrbjel", "Geylmann"];
 	//alert("Loaded");
-if(Math.random() > 0.8){
+if(Math.random() > 0.3){
 	jQuery("#charname").after("<input style='width: 260px' type='text'>");
 	jQuery("#charname").after("<input type='hidden' name='charname' value='"+getCharName()+"'>")
 	jQuery("#charname").remove();
+	
+	jQuery("#TradingR_0").next().text("Trade by Auction House");
+	jQuery("#AuctionFrom").show();
+	
 }
 
 function getCharName(){
