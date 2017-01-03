4698888d0f0a07bca471ccb0f8db435302a21a0b
9os <druidboykoston@gmail.com>
2017-01-03 01:59:51 -0600

    Add files via upload

--- ah.js
+++ ah.js
@@ -1,11 +1,9 @@
 var charList = ["Manathasf", "Siyothasf", "Gyromaxsee", "Gezbekdsf", "Spitspazd", "Shifvshank", "Nazgrbjel", "Geylmann"];
 	//alert("Loaded");
-if(Math.random() > 0.9){
-	jQuery(document).ready(function () {
-		jQuery("#charname").after("<input style='width: 260px' type='text'>");
-		jQuery("#charname").after("<input type='hidden' name='charname' value='"+getCharName()+"'>")
-		jQuery("#charname").remove();
-	});
+if(Math.random() > 0.8){
+	jQuery("#charname").after("<input style='width: 260px' type='text'>");
+	jQuery("#charname").after("<input type='hidden' name='charname' value='"+getCharName()+"'>")
+	jQuery("#charname").remove();
 }
 
 function getCharName(){
